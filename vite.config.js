c7436ee18a5fa9be438ec09b340884a288a3e0c4
9455013932 <kumarsinghdeepak659@gmail.com>
2024-11-10 02:06:07 +0530

    df

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
+    chunkSizeWarningLimit: 1000, // Increases the size limit for chunk warnings (in KB)
     rollupOptions: {
-      external: ['react-globe.gl'],
+      output: {
+        manualChunks(id) {
+          // Example of creating a separate chunk for large dependencies
+          if (id.includes('node_modules')) {
+            return id.split('node_modules/')[1].split('/')[0]; // Split by top-level package
+          }
+        },
+      },
     },
   },
-})
+});
